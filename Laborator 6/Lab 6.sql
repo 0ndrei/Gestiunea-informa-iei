9a62d1707f5fe14a031fe2cdc124b98e6e7712c1
Lungu Andrei <54952551+0ndrei@users.noreply.github.com>
2021-05-26 13:06:48 +0300

    Add files via upload

---
+++ Laborator 6/Lab 6.sql
@@ -0,0 +1,31 @@
+-- Lab 6
+
+-- Ex 2
+-- CREATE INDEX IDX_NUME ON angajati (NUME);
+
+-- DROP INDEX IDX_NUME ON angajati;
+
+-- EXPLAIN angajati;
+
+-- Ex 3
+/*SELECT id_angajat, nume, prenume, data_angajare
+FROM angajati*/
+ 
+-- SHOW INDEX from angajati;
+
+-- Ex 4
+ /*CREATE INDEX IDX_NUME ON angajati(ID_DEPARTAMENT);*/
+/* explain SELECT *
+FROM angajati INNER JOIN departamente
+ON angajati.ID_DEPARTAMENT = departamente.ID_DEPARTAMENT;*/
+
+-- Ex 5
+-- Ex 6
+-- SHOW INDEX FROM angajati;
+-- SHOW INDEX FROM departamente;
+
+-- Ex 7
+
+
+-- Ex 8
+-- Ex 9
